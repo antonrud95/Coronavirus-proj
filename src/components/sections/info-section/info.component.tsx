fd0ff828ca79bda04020b7301be8496df9097f26
arud95 <antonrud95@yandex.ru>
2021-01-03 18:15:17 +0200

    feaature: add info ornaments

--- src/components/sections/info-section/info.component.tsx
+++ src/components/sections/info-section/info.component.tsx
@@ -3,6 +3,8 @@ import {Container, Row, Col} from 'react-bootstrap'
 import InfoItem from '~/components/ui/general/info-item/info-item.component'
 import FC from '~/types/fc'
 import {Info} from '~/types/info.type'
+import useVirusAssets from '~/hooks/queries/useVirusAssets'
+import Img from 'gatsby-image'
 
 import styles from './info.module.scss'
 
@@ -11,6 +13,7 @@ interface Props {
 }
 
 const InfoSection: FC<Props> = ({information}) => {
+    const {virusImageSeventh, virusImageEighth, virusImageNineth} = useVirusAssets()
     return(
         <Container fluid className={styles.fluidContainer}>
             <div className={styles.infoHeader}>
@@ -18,6 +21,9 @@ const InfoSection: FC<Props> = ({information}) => {
                 <h2 className={styles.infoTitle}>What should we do</h2>
                 <p className={styles.infoText}> Corona viruses are a type of virus. There are many different kinds, and some cause disease. A newly identified type has caused</p>
             </div>
+            <Img fluid={virusImageSeventh.childImageSharp.fluid} className={styles.imageSeven}/>
+            <Img fluid={virusImageEighth.childImageSharp.fluid} className={styles.imageEight}/>
+            <Img fluid={virusImageNineth.childImageSharp.fluid} className={styles.imageNine}/>
             <div>
                 {information.map((info: Info, index) => {
                     if(index === 0 || index === 2) {
