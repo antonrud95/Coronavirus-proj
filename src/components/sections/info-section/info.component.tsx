a6c8c8e3c713dd96b4071da303f24f8998a0226b
arud95 <antonrud95@yandex.ru>
2021-01-03 17:58:05 +0200

    wip: work with info ornaments

--- src/components/sections/info-section/info.component.tsx
+++ src/components/sections/info-section/info.component.tsx
@@ -12,19 +12,43 @@ interface Props {
 
 const InfoSection: FC<Props> = ({information}) => {
     return(
-        <div>
-            {information.map((info: Info, index) => {
-                return(
-                    <InfoItem 
-                        key={info.id}
-                        title={info.title}
-                        text={info.text}
-                        image={info.image.localFile.childImageSharp.fluid}
-                        number={info.number.localFile.childImageSharp.fluid}
-                    />
-                )
-            })}
-        </div>
+        <Container fluid className={styles.fluidContainer}>
+            <div className={styles.infoHeader}>
+                <h3 className={styles.infoUppertitle}>Covid-19</h3>
+                <h2 className={styles.infoTitle}>What should we do</h2>
+                <p className={styles.infoText}> Corona viruses are a type of virus. There are many different kinds, and some cause disease. A newly identified type has caused</p>
+            </div>
+            <div>
+                {information.map((info: Info, index) => {
+                    if(index === 0 || index === 2) {
+                        return(
+                            <InfoItem 
+                                key={info.id}
+                                title={info.title}
+                                text={info.text}
+                                image={info.image.localFile.childImageSharp.fluid}
+                                number={info.number.localFile.childImageSharp.fluid}
+                                rowStyles={styles.infoRow__reversed}
+                                imageStyles={styles.rightImage}
+                            />
+                        )
+                    } else {
+                        return(
+                            <InfoItem 
+                                key={info.id}
+                                title={info.title}
+                                text={info.text}
+                                image={info.image.localFile.childImageSharp.fluid}
+                                number={info.number.localFile.childImageSharp.fluid}
+                                rowStyles={styles.infoRow}
+                                imageStyles={styles.leftImage}
+                            />
+                        )
+                    }
+                    
+                })}
+            </div>
+        </Container>
     )
 }
 
--- src/components/ui/general/info-item/info-item.component.tsx
+++ src/components/ui/general/info-item/info-item.component.tsx
@@ -10,22 +10,24 @@ interface Props {
     image: any 
     title: string
     text: string
+    rowStyles?: string
+    imageStyles?: string
 }
 
-const InfoItem: FC<Props> = ({number, image, title, text}) => {
+const InfoItem: FC<Props> = ({number, image, title, text, rowStyles, imageStyles}) => {
     return(
         <Container>
-            <Row>
+            <Row className={rowStyles}>
                 <Col xs="12" lg="6">
-                    <Img fluid={image}/>
+                    <Img fluid={image} className={imageStyles}/>
                 </Col>
-                <Col xs="12" lg="6">
-                    <div>
-                        <Img fluid={number}/>
+                <Col xs="12" lg="6" className={styles.contentWrapper}>
+                    <div className={styles.numberWrapper}>
+                        <Img fluid={number} className={styles.numberImage}/>
                     </div>
                     <div>
-                        <h3>{title}</h3>
-                        <p>{text}</p>
+                        <h3 className={styles.infoTitle}>{title}</h3>
+                        <p className={styles.infoText}>{text}</p>
                     </div>
                 </Col>
             </Row>
